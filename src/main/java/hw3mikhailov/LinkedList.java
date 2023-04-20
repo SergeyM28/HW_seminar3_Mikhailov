f585e1cda78f0fdd4bc5b43dc24d77cc9c78e330
SergeiM <mihailovsv@mail.ru>
2023-04-20 19:11:46 +0300

    HW seminar 3 by mikhailov done

--- src/main/java/hw3mikhailov/LinkedList.java
+++ src/main/java/hw3mikhailov/LinkedList.java
@@ -1,2 +1,44 @@
-package hw3mikhailov;public class LinkedList {
+package hw3mikhailov;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+
+    public class Node {
+        int value;
+        Node next;
+        Node previous;
+    }
+    public void addFirst(int value) {
+        Node node = new Node();
+        node.value = value;
+
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            head.previous = node;
+            node.next = head;
+            head = node;
+        }
+    }
+
+    public void printList (){
+        Node currentNode = head;
+        while (currentNode != null){
+            System.out.print(currentNode.value + " ");
+            currentNode = currentNode.next;
+        }
+    }
+    public void reverseList(){
+        Node currentNode = head;
+        Node tempNode = new Node();
+        while (currentNode != null){
+            head = currentNode;
+            currentNode = currentNode.next;
+            tempNode = head.previous;
+            head.previous = head.next;
+            head.next = tempNode;
+        }
+    }
 }
--- src/main/java/hw3mikhailov/Main.java
+++ src/main/java/hw3mikhailov/Main.java
@@ -1,5 +1,16 @@
-package hw3mikhailov;public class Main {
+package hw3mikhailov;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList myList = new LinkedList();
+        myList.addFirst(1);
+        myList.addFirst(2);
+        myList.addFirst(3);
+        myList.addFirst(4);
+
+        myList.printList();
+        myList.reverseList();
+        System.out.println("");
+        myList.printList();
     }
 }
